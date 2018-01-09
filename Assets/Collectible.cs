fc7a7ddb22f97f65bdf5e5f3e3683c89f806e4ce
Evan Knebel <s179060@students.aie.edu.au>
2018-01-08 16:14:40 -0800

    initial commit

---
+++ Assets/Collectible.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //get ballcontroller from the collision
+        BallController ballController = other.GetComponent<BallController>();
+        //add score
+     //   ballController.score += 1;
+        //destroy this object
+        Destroy(gameObject);
+    }
+}
